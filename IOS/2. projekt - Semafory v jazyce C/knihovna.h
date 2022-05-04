ff62aff2e366900e85a17e9a09eddefc4291717d
Michal Řezník <72881212+Wooo99@users.noreply.github.com>
2022-05-04 18:52:45 +0200

    Add files via upload

---
+++ IOS/2. projekt - Semafory v jazyce C/knihovna.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<sys/mman.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<semaphore.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<stdbool.h>
